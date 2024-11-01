f8470e194630be4f1570e84386eec0e913d39dbc
electrixky <tanyatkachuk2401@gmail.com>
2024-11-01 18:49:41 +0200

    multi-selected accordion

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,26 +1,49 @@
 import {useState} from "react";
 import data from "./data";
 import "./styles.css"
+import {IoIosArrowDown, IoIosArrowUp} from "react-icons/io";
 
 export default function Accordion() {
     const [selected, setSelected] = useState(null)
+    const [enableMulti, setEnableMulti] = useState(false)
+    const [multi, setMulti] = useState([])
 
     const handleSingleSelection = (id) => {
         setSelected(id === selected ? null : id)
     }
 
+    const handleMultiSelection = (id) => {
+        let copyMulti = [...multi]
+        const findIndexOfCurrentId = copyMulti.indexOf(id)
+        if (findIndexOfCurrentId === -1) {
+            copyMulti.push(id)
+        } else {
+            copyMulti.splice(findIndexOfCurrentId, 1)
+        }
+        setMulti(copyMulti)
+    }
+
     return <div className="wrapper">
+        <button
+            className={`btn ${enableMulti ? "disable-btn" : ""}`}
+            onClick={() => setEnableMulti(!enableMulti)}
+        >{`${enableMulti ? "Disable" : "Enable"} Multi Selection`}
+        </button>
         <div className="accordion">
             {
                 data && data.length > 0
                     ? data.map(item => {
-                        return <div className="item" onClick={() => handleSingleSelection(item.id)}>
+                        return <div className="item"
+                                    onClick={enableMulti
+                                        ? () => handleMultiSelection(item.id)
+                                        : () => handleSingleSelection(item.id)}>
                             <div className="title">
                                 <h3>{item.question}</h3>
-                                <span>+</span>
+                                <span className="toggle-btn">{selected === item.id ? <IoIosArrowUp/> :
+                                    <IoIosArrowDown/>}</span>
                             </div>
                             {
-                                selected === item.id
+                                selected === item.id || multi.indexOf(item.id) !== -1
                                     ? <div className="content">{item.answer}</div>
                                     : null
                             }
