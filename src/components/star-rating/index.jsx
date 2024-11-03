e83c3e7c87623d8cb3aa8eee57a5a84c8e81d9a9
electrixky <tanyatkachuk2401@gmail.com>
2024-11-03 20:09:04 +0200

    stars mouse hove and click logic

---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {FaStar} from "react-icons/fa";
+import "./styles.css"
+
+export const StarRating = ({numberOfStars = 5}) => {
+
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex)
+    }
+
+    function handleMouthEnter(getCurrentIndex) {
+        setHover(getCurrentIndex)
+    }
+
+    function handleMouthLeave(getCurrentIndex) {
+        setHover(rating)
+    }
+
+    return (
+        <div className="star-rating">
+            {
+                [...Array(numberOfStars)].map((star, index) => {
+                    index += 1
+
+                    return <FaStar
+                        key={index}
+                        onClick={() => handleClick(index)}
+                        onMouseMove={() => handleMouthEnter(index)}
+                        onMouseLeave={() => handleMouthLeave(index)}
+                        className={`star ${index <= (hover || rating) ? "active" : ""}  `}
+                    />
+                })
+            }
+        </div>
+    );
+};
