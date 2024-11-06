77ef7ef2c7369917d055a06eedef23b8a4f7ec84
electrixky <tanyatkachuk2401@gmail.com>
2024-11-06 09:06:26 +0200

    image slider component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Accordion from "./components/accordion";
 import RandomColor from "./components/random-color";
 import {StarRating} from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
     return (
         <div className="App">
             {/*<Accordion/>*/}
             {/*<RandomColor/>*/}
-            <StarRating numberOfStars={10}/>
+            {/*<StarRating numberOfStars={10}/>*/}
+            <ImageSlider/>
         </div>
     );
 }
