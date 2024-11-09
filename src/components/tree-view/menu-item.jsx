787b5de6c72d8102790987606e422d6d83f69c9b
electrixky <tanyatkachuk2401@gmail.com>
2024-11-09 13:19:32 +0200

    expanding tree logic

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,10 +1,33 @@
 import MenuList from "./menu-list";
+import {useState} from "react";
+import {FaMinus, FaPlus} from "react-icons/fa";
 
 export default function MenuItem({item}) {
-    return <li>
-        <p>{item.title}</p>
+
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({})
+
+    function handleToggleChildren(getCurrentTitle) {
+        setDisplayCurrentChildren({
+            ...displayCurrentChildren, [getCurrentTitle]: !displayCurrentChildren[getCurrentTitle]
+        })
+    }
+
+    return <li className="menu-item">
+        <div style={{display: "flex", gap: "20px"}}>
+            <p>{item.title}</p>
+            {
+                item && item.children && item.children.length
+                    ? <span onClick={() => handleToggleChildren(item.title)}>
+                    {
+                        displayCurrentChildren[item.title] ? <FaMinus color="#fff" size={25}/> :
+                            <FaPlus color="#fff" size={25}/>
+                    }
+                    </span>
+                    : null
+            }
+        </div>
         {
-            item && item.children && item.children.length > 0
+            (item && item.children && item.children.length > 0) && displayCurrentChildren[item.title]
                 ? <MenuList list={item.children}/>
                 : null
 
--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -1,10 +1,11 @@
 import MenuItem from "./menu-item";
+import "./styles.css"
 
 export default function MenuList({list = []}) {
     return <ul className="menu-list-container">
         {
             list && list.length
-                ? list.map(item=>{
+                ? list.map(item => {
                     return <MenuItem item={item}/>
                 })
                 : null
