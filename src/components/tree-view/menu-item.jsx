dac691d9a7f276001761b8a30241ec6f6aa6bfe6
electrixky <tanyatkachuk2401@gmail.com>
2024-11-09 13:29:47 +0200

    styling

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -12,15 +12,15 @@ export default function MenuItem({item}) {
         })
     }
 
-    return <li className="menu-item">
-        <div style={{display: "flex", gap: "20px"}}>
+    return <li>
+        <div className="menu-item">
             <p>{item.title}</p>
             {
                 item && item.children && item.children.length
                     ? <span onClick={() => handleToggleChildren(item.title)}>
                     {
-                        displayCurrentChildren[item.title] ? <FaMinus color="#fff" size={25}/> :
-                            <FaPlus color="#fff" size={25}/>
+                        displayCurrentChildren[item.title] ? <FaMinus color="#fff" size={20}/> :
+                            <FaPlus color="#fff" size={20}/>
                     }
                     </span>
                     : null
