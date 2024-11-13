dd473d4a4a15fbd5b4f14417a438f3bfead8c81b
electrixky <tanyatkachuk2401@gmail.com>
2024-11-13 19:54:49 +0200

    QrCode generator

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
+import QrCodeGenerator from "./components/qr-code-generator";
 
 function App() {
     return (
@@ -15,7 +16,8 @@ function App() {
             {/*<StarRating numberOfStars={10}/>*/}
             {/*<ImageSlider url={"https://picsum.photos/v2/list"} limit={"5"} page={"1"}/>*/}
             {/*<LoadMoreData/>*/}
-            <TreeView menus={menus}/>
+            {/*<TreeView menus={menus}/>*/}
+            <QrCodeGenerator/>
         </div>
     );
 }
